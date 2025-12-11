e58a43306ab8ea99bc05dc0be3a50203470b9890
chanukpark <pcu8339@gmail.com>
2025-12-11 23:57:03 +0900

    added gemini, zone_cube_datafill

--- auto_cube_delivery/core.py
+++ auto_cube_delivery/core.py
@@ -5,6 +5,10 @@ import math
 from auto_cube_delivery.modules.navigator import Navigator
 from auto_cube_delivery.modules.database import Database
 
+from auto_cube_delivery.modules.plan_generator import create_robot_plan, parse_final_plan
+from auto_cube_delivery.modules.zone_cube_analyzer import ZoneCubeAnalyzer
+
+
 def core_process():
     print("Starting Core Process...")
 
@@ -32,7 +36,11 @@ def core_process():
     # Add landmark coordinate
     database = Database(left = landmark[0], middle=landmark[1], right=landmark[2])
 
-    # Init Gemini
+    # Init Gemini ##
+    analyzer = ZoneCubeAnalyzer(
+        image_path="/home/ubuntu/LLM_Planning/capture/capture.jpg",
+        wait_before_capture=1.0,  # 필요하면 0.5나 0으로 조정 가능
+    )
     # gemini = Gemini()
 
     # Visiting Order
@@ -50,9 +58,19 @@ def core_process():
         if navigation_is_done:
             print(f"Arrived at Landmark: {direction}")
 
+        ##################################################
+        ### 여기서 로봇이 대가리를 안들어도 사진이 잘찍히겠죠? 아니면 여기서 대가리를 들면 될 것 같아요 ###
+
+        zone_num, cube_color = analyzer()
+        print(f"[Gemini] direction={direction}, zone={zone_num}, cube={cube_color}")
+
+        database.fill_zone_num(direction, zone_num)
+        if cube_color is not None:
+            database.fill_cube_info(cube_color, direction)
+
             # Move Arm to Marker Reading Position
 
-            # Read Image
+            # Read Image - 이미지 어떻게 가져오지??
 
             # if use_zone_marker:
                 # using aruco marker for zone detection
@@ -95,6 +113,19 @@ def core_process():
 
     # Ask Gemini for Taks action sequence
         # task_seq = gemini.ask_task(task_prompt)
+    environment_state = database.to_environment_state()
+    print("Environment State:", environment_state)
+    task_instruction = input("Enter task instruction: ")
+
+    plan_text = create_robot_plan(environment_state, task_instruction)
+    print("Gemini Response:\n", plan_text)
+    
+    task_seq = parse_final_plan(plan_text)
+    print("Parsed Task Sequence:", task_seq)
+
+    database.fill_task_seq(task_seq)
+
+
     # Assert type=list, all elements are in action options
     # Fill database with action sequence
         # database.fill_task_seq(task_seq)
